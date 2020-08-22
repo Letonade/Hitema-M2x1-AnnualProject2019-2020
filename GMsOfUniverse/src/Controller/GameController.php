01b051bf36418404c48e8a4b443f13fb75a4849e
Arinouchkine <arinouchkine@gmail.com>
2020-08-22 11:40:04 +0200

    save_state_02

---
+++ GMsOfUniverse/src/Controller/GameController.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class GameController extends AbstractController
+{
+
+    /**
+     * @Route("/game/get_Calendar", name="getCalendar", methods={"POST"})
+     */
+    public function getCalendar(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/game/create_campagne", name="createCampagne", methods={"POST"})
+     */
+    public function createCampagne(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/game/create_game", name="createGame", methods={"POST"})
+     */
+    public function createGame(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/game/participe", name="participe", methods={"POST"})
+     */
+    public function participe(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/game/no_participe", name="noParticipe", methods={"POST"})
+     */
+    public function noParticipe(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+
+
+}
---
+++ GMsOfUniverse/src/Controller/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/post/create_post", name="createPost", methods={"POST"})
+     */
+    public function createPost(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/post/modify_post", name="modifyPost", methods={"POST"})
+     */
+    public function modifyPost(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/post/get_slider", name="getSlider", methods={"POST"})
+     */
+    public function getSlider(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+    /**
+     * @Route("/post/get_historique", name="getHistorique", methods={"POST"})
+     */
+    public function getHistorique(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+
+    /**
+     * @Route("/post/get_post", name="getPost", methods={"POST"})
+     */
+    public function getPost(Request $request)
+    {
+        $user = $this->getUser();
+
+        return new JsonResponse(['value' =>'create_post'], 200);
+    }
+
+}
