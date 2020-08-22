d6717fd2dc9fe3c83114d2e3f7b53e6d0699f604
Arinouchkine <arinouchkine@gmail.com>
2020-08-22 15:27:32 +0200

    save_state_02.3

--- GMsOfUniverse/config/bundles.php
+++ GMsOfUniverse/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
