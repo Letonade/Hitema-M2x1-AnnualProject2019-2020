bdb9813dba87b6055958fb3422c75307a2a06f2b
Letonade <cocoscorpio94@hotmail.fr>
2020-08-29 11:36:34 +0200

    Ajout du service pour les game et campagne (à remplir)

---
+++ GMsOfUniverse_React/gmsofuniverse_react/src/services/organisator.service.js
@@ -0,0 +1,12 @@
+const apiUrl = "http://127.0.0.1:8000";
+
+class OrganisatorService {
+
+	static async funckyLaFonctionQuiFunk () {
+		console.log("Funk Funk .Funk ..Funk FunkFunk Fk..");
+	}
+
+	
+}
+
+export default OrganisatorService;
