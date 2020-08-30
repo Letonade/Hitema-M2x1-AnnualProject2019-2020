ce6018f7f8a05ef0e1eda99a9c3d8653cca609b0
Arinouchkine <arinouchkine@gmail.com>
2020-08-30 13:30:02 +0200

    save_state_02.8.5

--- GMsOfUniverse/src/Controller/PostController.php
+++ GMsOfUniverse/src/Controller/PostController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Post;
+use App\Entity\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,8 +19,25 @@ class PostController extends AbstractController
     public function createPost(Request $request)
     {
         $user = $this->getUser();
+        $values = json_decode($request->getContent());
 
-        return new JsonResponse(['value' =>'create_post'], 200);
+
+        if(isset($values->type_id,$values->name,$values->content,$values->date->date,$values->sponsored))
+        {
+            $post = new Post();
+
+            $post->setName($values->name);
+            $post->setContent($values->content);
+            $post->setSponsored($values->sponsored);
+            $post->setDate(new \DateTime($values->date));
+            $post->setUserId($user);
+            $post->setType($this->getDoctrine()->getRepository(Type::class)->find($values->type_id));
+
+
+            return new JsonResponse(['value' =>'create_post'], 200);
+        }
+
+        return new JsonResponse(['value' => 'bad json. You are a bad boy, bad json bad boy'], 500);
     }
 
     /**
@@ -32,7 +51,7 @@ class PostController extends AbstractController
     }
 
     /**
-     * @Route("/post/get_slider", name="getSlider", methods={"POST"})
+     * @Route("/post/get_slider", name="getSlider", methods={"GET"})
      */
     public function getSlider(Request $request)
     {
@@ -42,7 +61,7 @@ class PostController extends AbstractController
     }
 
     /**
-     * @Route("/post/get_historique", name="getHistorique", methods={"POST"})
+     * @Route("/post/get_historique", name="getHistorique", methods={"GET"})
      */
     public function getHistorique(Request $request)
     {
@@ -53,7 +72,7 @@ class PostController extends AbstractController
 
 
     /**
-     * @Route("/post/get_post", name="getPost", methods={"POST"})
+     * @Route("/post/get_post", name="getPost", methods={"GET"})
      */
     public function getPost(Request $request)
     {
