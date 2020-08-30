d437ad24a9751aa14568cdfac461b96dc8f8e7b3
Arinouchkine <arinouchkine@gmail.com>
2020-08-30 21:47:22 +0200

    save_state_02.8.8.5

--- GMsOfUniverse_React/gmsofuniverse_react/src/services/post.service.js
+++ GMsOfUniverse_React/gmsofuniverse_react/src/services/post.service.js
@@ -1,8 +1,99 @@
-const apiUrl = "http://127.0.0.1:8000";
+const apiUrl = "http://127.0.0.1:8000/post/";
 
 class PostService {
 
-	
+
+    static async createPost (body) {
+        let init = {
+            method : "POST",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/create_post`, init)
+        return call;
+    }
+
+    static async modifyPost (body) {
+        let init = {
+            method : "POST",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/modify_post`, init)
+        return call;
+    }
+
+    static async modifyPostImg (body) {
+        let init = {
+            method : "POST",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/modify_post_img`, init)
+        return call;
+    }
+
+    static async getSlider (body) {
+        let init = {
+            method : "GET",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/get_slider`, init)
+        return call;
+    }
+
+    static async getHistorique (body) {
+        let init = {
+            method : "GET",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/get_historique`, init)
+        return call;
+    }
+
+    static async getSponso (body) {
+        let init = {
+            method : "GET",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/get_sponso`, init)
+        return call;
+    }
+
+    static async getPost (body) {
+        let init = {
+            method : "POST",
+            headers : {
+                'Authorization': 'Bearer ' + localStorage.getItem("TokenUser"),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        let call = await fetch(`${apiUrl}/get_post`, init)
+        return call;
+    }
+
 }
 
 export default PostService;
